53022cebf021a0c91aa10d6ca93b45ef9ec36e63
h584903 <584903@stud.hvl.no>
2023-12-13 12:54:44 +0100

    Added simple template and added a timer

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,13 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Game from "./components/Game.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <h1>Welcome to doomsday!</h1>
+    <p>This game is based on the doomsday algorithym, the calculation of this is run by a haskell program in the background while the front-end is powered by vue and JS</p>
+    <p>If you need a refresher on the alogorythm you can click <a href>this</a> to freshen up on the rules</p>
+    <Game />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
---
+++ src/components/Game.vue
@@ -0,0 +1,37 @@
+<script setup>
+import {onUnmounted, ref} from "vue"
+
+const initialTime = 60;
+const timeLeft = ref(initialTime);
+const timer = ref(null);
+
+function startTime() {
+    clearInterval(timer.value);
+    timeLeft.value = initialTime;
+
+    timer.value = setInterval(() => {
+        if (timeLeft.value > 0) {
+            timeLeft.value -= 1;
+        } else {
+            clearInterval(timer)
+            timer=null;
+            // logic for finished time
+        }
+    }, 1000);
+}
+
+onUnmounted(()=> {
+    if (timer.value) {
+        clearInterval(timer.value);
+    }
+})
+
+</script>
+<template>
+    <h1 id="timer">{{ timeLeft }}</h1>
+    <span id="question"></span>
+    <input id="user-input">
+    <button id="start-button" @click="startTime">Start</button>
+</template>
+<style>
+</style>
